824017bee4ae9f8792c676bae2e227703f409b8f
Don <dbrandon0213@gmail.com>
2020-08-03 17:15:43 -0500

    clearing the directory

---
+++ angular_practice/firstProject/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'The Report';
+  name = "Don Brandon";
+  itemList = ["item1", "item2", "item3", "item4"];
+  rectangle = {
+    length: 5,
+    width: 6,
+    area: function() {
+       return this.length * this.width;
+    }
+  };
+}
---
+++ hello_world.ts
@@ -0,0 +1,3 @@
+let message: string = "Hello World";
+console.log(message);
+console.log("This is a typescript file. To run, open the terminal, then complie to javaScript using the command 'tsc hello_world.ts'. This creates an new .js file that can be run in the terminal by the command 'node hello_world.ts'. This will find errors and print them in the terminal.")
